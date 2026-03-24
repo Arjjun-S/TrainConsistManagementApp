8d162fd52db3c7383fe884c826cccfde84c71d02
Arjjun-S <suresharjjun@gmail.com>
2026-03-24 09:23:02 +0530

    UC1: Initialize Train and Display Consist

--- src/TrainConsistMgmnt.java
+++ src/TrainConsistMgmnt.java
@@ -7,11 +7,6 @@ public class TrainConsistMgmnt {
         List<String> trainConsist = new ArrayList<>();
         System.out.println("Train initialized successfully...");
         System.out.println("Initial Bogie Consist : "+trainConsist.size());
-        System.out.print("Current Train Consist : [");
-        for(int i=0;i<trainConsist.size();i++)
-        {
-            System.out.print(trainConsist.get(i)+",");
-        }
-        System.out.print("]");
+        System.out.print("Current Train Consist : " + trainConsist);
     }
 }
